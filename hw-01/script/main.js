83aa2668d50259427a1864d0317e04f3b3ffedba
NataliiaLysenkova <lsnkv9ntl@gmail.com>
2024-09-14 01:42:27 +0300

    first-commit

---
+++ hw-01/script/main.js
@@ -0,0 +1,18 @@
+let userNickname = "Nataliia";
+console.log(userNickname);
+let userScore = 5;
+console.log(userScore);
+let age = 35;
+console.log(age);
+age += 5;
+console.log(age);
+let isStudent = true;
+console.log(isStudent);
+isStudent = false;
+console.log(isStudent);
+let item = 10;
+console.log(item);
+item = "ten";
+console.log(item);
+item = true;
+console.log(item);
